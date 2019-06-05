c3cbc9881457f9fc7609c4028d0d16ab2b32ace3
MakeDirtyCode <make.dirty.code@gmail.com>
2019-06-05 16:07:19 +0900

    default format

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from trainer import Trainer
 
 def main():
-    trainer = Trainer()
-    trainer.run()
+    dataloader = NotImplemented
+    trainer = Trainer(nz=512, lr=0.001, betas=(0.5, 0.999))
+    trainer.run(dataloader)
  
 if __name__ == '__main__':
     main()
--- trainer.py
+++ trainer.py
@@ -1,9 +1,53 @@
 from networks import Generator, Discriminator
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
 
 class Trainer:
-    def __init__(self):
+    def __init__(self, nz, lr, betas):
+        self.nz = nz
+
         self.generator = Generator()
         self.discriminator = Discriminator()
+
+        self.optimizer_g = optim.Adam(params=self.generator.parameters(), lr=lr, betas=betas)
+        self.optimizer_d = optim.Adam(params=self.discriminator.parameters(), lr=lr, betas=betas)
     
-    def run(self):
-        pass
+    def generator_trainloop(self, batch_size):
+        z = torch.randn(batch_size, self.nz)
+
+        fake = self.generator(z)
+        d_fake = self.discriminator(fake)
+        loss = F.softplus(-d_fake).mean()
+
+        self.optimizer_g.zero_grad()
+        loss.backward()
+        self.optimizer_g.step()
+    
+    def discriminator_trainloop(self, real):
+        d_real = self.discriminator(real)
+        loss_real = F.softplus(-d_real).mean()
+
+        z = torch.randn(real.size(0), self.nz)
+
+        fake = self.generator(z)
+        d_fake = self.discriminator(fake)
+        loss_fake = F.softplus(d_fake).mean()
+
+        loss = loss_real + loss_fake
+
+        self.optimizer_d.zero_grad()
+        loss.backward()
+        self.optimizer_d.step()
+        
+
+    def run(self, dataloader):
+        global_iter = 0
+        while True:
+            for data in dataloader:
+                real = data.cuda()
+
+                self.discriminator_trainloop(real)
+                self.generator_trainloop(real.size(0))
+
+                global_iter += 1
